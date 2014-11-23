b10b67fe49052e9dc00001a72a16274fcc79d416
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-11-24 00:19:56 +0700

    Added index page and render it in `main_blueprint`

--- aikithoughts/blueprints/main/main.py
+++ aikithoughts/blueprints/main/main.py
@@ -1,6 +1,7 @@
+from flask.templating import render_template
 from aikithoughts.blueprints.main import main_blueprint
 
 
 @main_blueprint.route('/')
 def index():
-    return "Hello World!"
+    return render_template('index.html')
