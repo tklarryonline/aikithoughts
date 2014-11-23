574ec4ccdc53ca5841938c2ddc4f1d124151117b
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-11-24 00:46:05 +0700

    Allowed user to login with random OpenID

--- aikithoughts/blueprints/account/account.py
+++ aikithoughts/blueprints/account/account.py
@@ -1,9 +1,16 @@
+from flask.helpers import flash, url_for
 from flask.templating import render_template
+from werkzeug.utils import redirect
 from aikithoughts.blueprints.account import account_blueprint
 from aikithoughts.forms import LoginOpenIDForm
 
 
-@account_blueprint.route('/login/', methods=['GET'])
+@account_blueprint.route('/login/', methods=['GET', 'POST'])
 def login():
     form = LoginOpenIDForm()
+
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' % (form.openid.data, str(form.remember_me.data)))
+        return redirect(url_for('main.index'))
+
     return render_template('account/login.html', form=form)
