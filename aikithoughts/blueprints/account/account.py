515d0283e065194d7a71ceebb6d7beda0372b48d
Luan Nguyen Thanh <tklarryonline@gmail.com>
2014-12-01 00:56:21 +0700

    Added edit profile page

--- aikithoughts/blueprints/account/account.py
+++ aikithoughts/blueprints/account/account.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
-from flask.ext.login import login_required
+from flask.ext.login import login_required, current_user
 from flask.helpers import flash, url_for
 from flask.templating import render_template
 from werkzeug.utils import redirect
 
 from aikithoughts.blueprints.account import account_blueprint
+from aikithoughts.forms import EditProfileForm
 from aikithoughts.models.accounts import User
+from aikithoughts.runtime import db
 
 
 @account_blueprint.route('/profile/<id>')
@@ -17,3 +19,19 @@ def profile(id):
         return redirect(url_for('main.index'))
 
     return render_template('account/profile.html', user=user)
+
+
+@account_blueprint.route('/profile/edit', methods=['GET', 'POST'])
+@login_required
+def edit_profile():
+    form = EditProfileForm(obj=current_user)
+
+    if form.validate_on_submit():
+        form.populate_obj(current_user)
+        db.session.add(current_user)
+        db.session.commit()
+
+        flash('Updated your info')
+        return redirect(url_for('account.profile', id=current_user.id))
+
+    return render_template('account/edit_profile.html', form=form)
--- aikithoughts/forms.py
+++ aikithoughts/forms.py
@@ -1,10 +1,10 @@
 from flask.ext.wtf.form import Form
-from wtforms.fields.core import StringField, BooleanField
-from wtforms.fields.simple import SubmitField
-from wtforms.validators import DataRequired
+from wtforms.fields.core import StringField
+from wtforms.fields.simple import SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 
-class LoginOpenIDForm(Form):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
-    submit = SubmitField('Sign in')
+class EditProfileForm(Form):
+    nickname = StringField(label='nickname', validators=[DataRequired()])
+    about_me = TextAreaField(label='about me', validators=[Length(min=0, max=140)])
+    submit = SubmitField()
